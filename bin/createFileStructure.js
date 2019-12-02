5e03d0ad4747ed65037fd82815545a1b29221186
Andrew Shin <andyrpg@gmail.com>
2019-12-02 12:00:16 -0800

    Added notes to the createFile.js

--- bin/createFileStructure.js
+++ bin/createFileStructure.js
@@ -1,12 +1,9 @@
 #!/usr/bin/env node
 
 const inquirer = require("inquirer"); //a collection of common interactive command line user interfaces
-const chalk = require("chalk"); //terminal string styling done right
-const figlet = require("figlet"); // program for making large letters our of ordinary text
 const shell = require("shelljs"); // portable unix shell commands for node.js 
-const fs = require("fs");
+const fs = require("fs"); 
 
-//creating
 const createFolderStructure = () => {
     const srcText = `src`
     const modelsText = `models`
@@ -15,9 +12,9 @@ const createFolderStructure = () => {
 }
 
 const foldersInsideSrc = () => { 
-    const text = ['data', 'models', 'resolvers', 'typeDefs'];
-    text.forEach(element => {
-        let filepath = `${process.cwd()}/src/${element}.js`
+    const folders = ['data', 'models', 'resolvers', 'typeDefs'];
+    folders.forEach(element => {
+        let filepath = `${process.cwd()}/src/${element}`
         shell.mkdir(filepath);
     })
 }
--- bin/mongoDB.js
+++ bin/mongoDB.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
 const inquirer = require("inquirer"); //a collection of common interactive command line user interfaces
-const chalk = require("chalk"); //terminal string styling done right
-const figlet = require("figlet"); // program for making large letters our of ordinary text
 const shell = require("shelljs"); // portable unix shell commands for node.js 
 const fs = require("fs");
 
+//NOTE: at the moment, you need to already have invoked the createFileStructure command:
+//function that asks if user has a mongoDB. If they do, ask for their schema and add it to the src/models folder. If not, ask for the name of their mongoDB.
 const doesDbExist = () => { 
 inquirer.prompt({
     name: "MONGODB",
@@ -14,16 +14,43 @@ inquirer.prompt({
 })
 .then( answers => {
     if (answers.MONGODB === true) { 
-        inquirer.prompt({
+        const questions = [{
             name:"URL",
             message: "Please enter the MongoDB url: ",
             type: "input",
+        },
+        { 
+            name: "SCHEMA",
+            message: "Copy and paste your MongoDB schema into your text editor: ",
+            type: 'editor',
+        }]
+     
+        inquirer.prompt(questions)
+        .then( answers => {
+            const filePath = `src/models/schema.js`;
+            shell.touch(filePath);
+            fs.writeFile(filePath, answers.SCHEMA, err => {
+                console.log(err);
+            })
         })
     } else {
         inquirer.prompt({
-            name:"DATABASE",
+            name:"DATABASENAME",
             message: "What do you want to call your MongoDB database?",
             type: "input"
+        })
+        .then( answers => {
+            const filePath = `src/models/schema.js`;
+            const mongoDBText = `mongoose.connect(mongo_uri, { dbName: "${answers.DATABASENAME}"})`;
+
+            shell.touch(filePath);
+            fs.writeFile(filePath, mongoDBText, err => {
+                if (err) { 
+                    return console.log("this is the error: " ,err);
+                } else { 
+                    return;
+                } 
+            })
         })
     }
 })
@@ -34,7 +61,7 @@ inquirer.prompt({
 
 const run = async() => { 
     doesDbExist();
-    // askForMongoDBUrl();
+    
 };
 
 run();

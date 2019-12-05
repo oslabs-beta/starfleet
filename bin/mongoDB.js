959e0635213a8dca4a17a1c7880c6f6c9964b99b
Jason Yu <jyu.json@gmail.com>
2019-12-05 10:24:43 -0800

    bin changes merge

--- bin/mongoDB.js
+++ bin/mongoDB.js
@@ -3,7 +3,32 @@
 // feeling cute might delete later
 // might not need this file
 
+<<<<<<< .merge_file_a14516
 // const inquirer = require("inquirer"); //a collection of common interactive command line user interfaces
+=======
+const doesDbExist = () => { 
+inquirer.prompt({
+    name: "MONGODB",
+    message: chalk.blue("Do you have a existing MongoDB table?"),
+    type: "confirm",
+})
+.then( answers => {
+    if (answers.MONGODB === true) { 
+        inquirer.prompt({
+            name:"URL",
+            message: "Please enter the MongoDB url: ",
+            type: "input",
+        })
+    } else {
+        inquirer.prompt({
+            name:"DATABASE",
+            message: "What do you want to call your MongoDB database?",
+            type: "input"
+        })
+    }
+})
+};
+>>>>>>> .merge_file_a09600
 
 // const doesDbExist = () => { 
 // inquirer.prompt({

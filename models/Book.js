5f35fe6547810a4cf77c95fd7b1e70d634970e77
Jason Yu <jyu.json@gmail.com>
2019-12-05 10:30:58 -0800

    models schema matched

--- models/Book.js
+++ models/Book.js
@@ -3,13 +3,13 @@ const Schema = mongoose.Schema;
 
 const BookSchema = new Schema({
   name: {
-    type: String,
-    required: true
+	type: String,
+	required: true
   },
   author: {
-    type: String,
-    required: true
+	type: String,
+	required: true
   }
 });
-const model = mongoose.model('book', BookSchema);
-module.exports = model;
+
+module.exports = mongoose.model('book', BookSchema);

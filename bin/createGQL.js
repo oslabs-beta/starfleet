adc10d05be6974afc835ad1301276779ea24eed5
justin1j <wonjun110@gmail.com>
2019-12-04 18:11:51 -0800

    porting graphqlSchema Object directly method

--- bin/createGQL.js
+++ bin/createGQL.js
@@ -11,24 +11,24 @@ const createGQL = (model, modelName) => {
   const ModelTC = composeWithMongoose(model, customizationOptions);
 
   schemaComposer.Query.addFields({
-		[modelName+"ById"] : ModelTC.getResolver('findById'),
-		[modelName+"ByIds"] : ModelTC.getResolver('findByIds'),
-		[modelName+"One"] : ModelTC.getResolver('findOne'),
-		[modelName+"Many"] : ModelTC.getResolver('findMany'),
-		[modelName+"Count"] : ModelTC.getResolver('count'),
-		[modelName+"Connection"] : ModelTC.getResolver('connection'),
-		[modelName+"Pagination"] : ModelTC.getResolver('pagination'),
+	[modelName+"ById"] : ModelTC.getResolver('findById'),
+	[modelName+"ByIds"] : ModelTC.getResolver('findByIds'),
+	[modelName+"One"] : ModelTC.getResolver('findOne'),
+	[modelName+"Many"] : ModelTC.getResolver('findMany'),
+	[modelName+"Count"] : ModelTC.getResolver('count'),
+	[modelName+"Connection"] : ModelTC.getResolver('connection'),
+	[modelName+"Pagination"] : ModelTC.getResolver('pagination'),
   });
 
   schemaComposer.Mutation.addFields({
-		[modelName+"CreateOne"] : ModelTC.getResolver('createOne'),
-		[modelName+"CreateMany"] : ModelTC.getResolver('createMany'),
-		[modelName+"UpdateById"] : ModelTC.getResolver('updateById'),
-		[modelName+"UpdateOne"] : ModelTC.getResolver('updateOne'),
-		[modelName+"UpdateMany"] : ModelTC.getResolver('updateMany'),
-		[modelName+"RemoveById"] : ModelTC.getResolver('removeById'),
-		[modelName+"RemoveOne"] : ModelTC.getResolver('removeOne'),
-		[modelName+"RemoveMany"] : ModelTC.getResolver('removeMany'),
+	[modelName+"CreateOne"] : ModelTC.getResolver('createOne'),
+	[modelName+"CreateMany"] : ModelTC.getResolver('createMany'),
+	[modelName+"UpdateById"] : ModelTC.getResolver('updateById'),
+	[modelName+"UpdateOne"] : ModelTC.getResolver('updateOne'),
+	[modelName+"UpdateMany"] : ModelTC.getResolver('updateMany'),
+	[modelName+"RemoveById"] : ModelTC.getResolver('removeById'),
+	[modelName+"RemoveOne"] : ModelTC.getResolver('removeOne'),
+	[modelName+"RemoveMany"] : ModelTC.getResolver('removeMany'),
   });
 
   let graphqlSchemaObj = schemaComposer.buildSchema();

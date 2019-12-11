1a204afc776d49b0717705fb8082050d8d6e1db5
justin1j <wonjun110@gmail.com>
2019-12-10 19:49:29 -0800

    added resolvers

--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const Tour = require('./models/tourModel');
 
 module.exports = resolvers = {
@@ -5,7 +6,50 @@ module.exports = resolvers = {
     tourModelById: async (_, id) => {
       const tour = await Tour.findById(id);
       return tour;
-    } 
+    },
+    tourModelByIds: async (_, ids) => {
+      const tours_ids = await ids._ids.map((id) => mongoose.Types.ObjectId(id));
+      const tours = await Tour.find({
+        _id: {
+          $in: tours_ids
+        }
+      });
+      return tours;
+    },
+    tourModelOne: async (_, arg) => {
+
+    },
+    tourModelMany: async () => {
+
+    },
+    tourModelCount: async () => {
+
+    }
+  },
+  Mutation: {
+    tourModelCreateOne: async (_, args) => {
+
+    },
+    tourModelCreateMany: async () => {
+
+    },
+    tourModelUpdateById: async (_, id) => {
+
+    },
+    tourModelUpdateOne: async () => {
+
+    },
+    tourModelUpdateMany: async () => {
+
+    },
+    tourModelRemoveById: async () => {
+
+    },
+    tourModelRemoveOne: async () => {
+
+    },
+    tourModelRemoveMany: async () => {
+
+    }
   }
 };
-
